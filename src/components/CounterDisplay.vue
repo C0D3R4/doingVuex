0631fd569e740a074be46a7132548cd456f964b8
Andres Grams <agl.c0d3r4@gmail.com>
2017-05-15 20:40:28 +0100

    vuex simple file

---
+++ src/components/CounterDisplay.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    Count is {{ count }}
+  </div>
+</template>
+
+<script>
+import store from '../store'
+
+export default {
+  computed: {
+    counter() {
+      return store.state.counter
+    }
+  }
+}
+</script>
+<style>
+</style>
